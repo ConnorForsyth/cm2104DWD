6779d15c56bf7451ec60c7997a5b1dae68eeec91
ConnorForsyth <connorforsyth34@gmail.com>
2018-02-10 18:50:06 +0000

    d

--- project/test.js
+++ project/test.js
@@ -1,7 +1,5 @@
 <script>
-var countryRequests = ['https://restcountries.eu/rest/v2/name/Argentina', 'https://restcountries.eu/rest/v2/name/France', 'https://restcountries.eu/rest/v2/name/Germany', 'https://restcountries.eu/rest/v2/name/Italy', 'https://restcountries.eu/rest/v2/name/Spain', 'https://restcountries.eu/rest/v2/name/Japan', 'https://restcountries.eu/rest/v2/name/Thailand', 'https://restcountries.eu/rest/v2/name/China'];
-
-var argentina = 'https://restcountries.eu/rest/v2/name/Argentina';
-
-document.getElementByID("jsonDemo").innerHTML = 5 + 6;
+//var countryRequests = ['https://restcountries.eu/rest/v2/name/Argentina', 'https://restcountries.eu/rest/v2/name/France', 'https://restcountries.eu/rest/v2/name/Germany', 'https://restcountries.eu/rest/v2/name/Italy', 'https://restcountries.eu/rest/v2/name/Spain', 'https://restcountries.eu/rest/v2/name/Japan', 'https://restcountries.eu/rest/v2/name/Thailand', 'https://restcountries.eu/rest/v2/name/China'];
+// var argentina = 'https://restcountries.eu/rest/v2/name/Argentina';
+document.getElementById("jsonDemo").innerHTML = 5 + 6;
 </script>
