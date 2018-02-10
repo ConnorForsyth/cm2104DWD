2b8c6dabae69d65ea0aa541dd53f25ac3246783c
ConnorForsyth <connorforsyth34@gmail.com>
2018-02-10 17:47:45 +0000

    Another one

--- project/test.js
+++ project/test.js
@@ -1,7 +1,6 @@
 <script>
-// When the user clicks on <div>, open the popup
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-}
+var countryRequests = ['https://restcountries.eu/rest/v2/name/Argentina', 'https://restcountries.eu/rest/v2/name/France', 'https://restcountries.eu/rest/v2/name/Germany', 'https://restcountries.eu/rest/v2/name/Italy', 'https://restcountries.eu/rest/v2/name/Spain', 'https://restcountries.eu/rest/v2/name/Japan', 'https://restcountries.eu/rest/v2/name/Thailand', 'https://restcountries.eu/rest/v2/name/China'];
+
+var argentina = 'https://restcountries.eu/rest/v2/name/Argentina';
+
 </script>
