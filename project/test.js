fa0b3859b4405cdeef3c8f7dfab8d9574e25857b
ConnorForsyth <connorforsyth34@gmail.com>
2018-02-12 15:55:12 +0000

    Fixed???

--- project/test.js
+++ project/test.js
@@ -1,6 +1,7 @@
 <script>
 //var countryRequests = ['https://restcountries.eu/rest/v2/name/Argentina', 'https://restcountries.eu/rest/v2/name/France', 'https://restcountries.eu/rest/v2/name/Germany', 'https://restcountries.eu/rest/v2/name/Italy', 'https://restcountries.eu/rest/v2/name/Spain', 'https://restcountries.eu/rest/v2/name/Japan', 'https://restcountries.eu/rest/v2/name/Thailand', 'https://restcountries.eu/rest/v2/name/China'];
 // var argentina = 'https://restcountries.eu/rest/v2/name/Argentina';
-var jsonAttempt = document.getElementByID("jsonDemo");
-jsonAttempt.innerHTML = "1,2,3,4,5,6,7,8,9,10";
+
+
+$("jsonDemo").innerHTML = "1,2,3,4,5,6,7,8,9,10";
 </script>
