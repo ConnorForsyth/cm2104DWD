5f6b9bfd5effe73edc3663df374b555e6bae71c2
ConnorForsyth <connorforsyth34@gmail.com>
2018-02-12 15:40:39 +0000

    First js attempt

--- project/test.js
+++ project/test.js
@@ -1,5 +1,6 @@
 <script>
 //var countryRequests = ['https://restcountries.eu/rest/v2/name/Argentina', 'https://restcountries.eu/rest/v2/name/France', 'https://restcountries.eu/rest/v2/name/Germany', 'https://restcountries.eu/rest/v2/name/Italy', 'https://restcountries.eu/rest/v2/name/Spain', 'https://restcountries.eu/rest/v2/name/Japan', 'https://restcountries.eu/rest/v2/name/Thailand', 'https://restcountries.eu/rest/v2/name/China'];
 // var argentina = 'https://restcountries.eu/rest/v2/name/Argentina';
-document.getElementById("jsonDemo").innerHTML = 5 + 6;
+var jsonAttempt = document.getElementByID("jsonDemo");
+jsonAttempt.innerHTML = "1,2,3,4,5,6,7,8,9,10";
 </script>
