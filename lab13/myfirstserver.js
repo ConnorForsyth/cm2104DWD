8d79c393ee3cc77c17c8237b0032e8f7675c697b
ConnorForsyth <connorforsyth34@gmail.com>
2018-03-26 12:13:19 +0100

    ...

--- lab13/myfirstserver.js
+++ lab13/myfirstserver.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createServer(funtion (req, res){
+http.createServer(funtion (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World');
-}) .listen(8080);
+}).listen(8080);
