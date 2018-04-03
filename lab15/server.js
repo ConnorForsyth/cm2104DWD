6200859f431c9e1074dc0fde17273f5955d98c1b
ConnorForsyth <connorforsyth34@gmail.com>
2018-04-03 23:46:49 +0100

    Woopa

--- lab15/server.js
+++ lab15/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
- res.send("Hello world! by express”);
-})
+ res.send("Hello world using express");
+});
 
 app.listen(8080);
